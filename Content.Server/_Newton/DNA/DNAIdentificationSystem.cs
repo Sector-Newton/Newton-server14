0694c1632f4c489318e79c23746eb2764bb3c6c3
Ivilasting <Scorplix9@gmail.com>
2026-08-30 19:39:49 +0300

    ops..

--- Content.Server/_Newton/DNA/DNAIdentificationSystem.cs
+++ Content.Server/_Newton/DNA/DNAIdentificationSystem.cs
@@ -23,12 +23,12 @@ namespace Content.Server.Newton.DNA;
 
 public sealed partial class IdentificationSystem : EntitySystem
 {
-    [Dependency] private readonly ChatSystem _chat = default!;
-    [Dependency] private readonly PopupSystem _popupSystem = default!;
-    [Dependency] private readonly IAdminLogManager _adminLogger = default!;
-    [Dependency] private readonly ExplosionSystem _explosionSystem = default!;
-    [Dependency] private readonly SharedTransformSystem _transformSystem = default!;
-    [Dependency] private readonly GibbingSystem _gibbing = default!;
+    [Dependency] private ChatSystem _chat = default!;
+    [Dependency] private PopupSystem _popupSystem = default!;
+    [Dependency] private IAdminLogManager _adminLogger = default!;
+    [Dependency] private ExplosionSystem _explosionSystem = default!;
+    [Dependency] private SharedTransformSystem _transformSystem = default!;
+    [Dependency] private GibbingSystem _gibbing = default!;
 
     public override void Initialize()
     {
